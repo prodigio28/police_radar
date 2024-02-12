d4591c0b749fab38c865b064e25865f24b26fadf
Gusta <lucas.azev27@gmail.com>
2024-02-12 19:27:33 -0300

    police_radar: bind default option added

--- src/general.lua
+++ src/general.lua
@@ -1,5 +1,6 @@
 main_settings = {
-    sound = "src/client/assets/sounds/beep.mp3",
+    bindToOpen = "n", -- BIND PARA ABRIR O RADAR NO CARRO POLICIAL
+    sound = "src/client/assets/sounds/beep.mp3", -- SOM AO UM CARRO PROCURADO PASSAR NO RADAR DO POLICIAL
     wanted = "wanted", -- ELEMENT DATA CARRO PROCURADO (SETADO NO VEÍCULO)
     patrol = "police", -- ELEMENT DATA CARRO POLICIAL (SETADO NO VEÍCULO; PARA ABRIR O RADAR)
     range = 15, -- ALCANCE DO RADAR PARA BUSCAR UM VEÍCULO (NA FRENTE E ATRÁS)
