d6007d4db50d56d23e3dfffde2b579b6afe9d092
Gusta <lucas.azev27@gmail.com>
2024-02-12 19:09:39 -0300

    Police_Radar upload

---
+++ src/general.lua
@@ -0,0 +1,6 @@
+main_settings = {
+    sound = "src/client/assets/sounds/beep.mp3",
+    wanted = "wanted", -- ELEMENT DATA CARRO PROCURADO (SETADO NO VEÍCULO)
+    patrol = "police", -- ELEMENT DATA CARRO POLICIAL (SETADO NO VEÍCULO; PARA ABRIR O RADAR)
+    range = 15, -- ALCANCE DO RADAR PARA BUSCAR UM VEÍCULO (NA FRENTE E ATRÁS)
+}
